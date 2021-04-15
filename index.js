ac2a6a1b2df1ff2f337cffc6d6afd619b1bac8fb
Samuel Adekoya <65285073+fluxstride@users.noreply.github.com>
2021-04-15 09:37:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,21 +9,22 @@ fetch(url)
       jsonString = JSON.stringify(json,null,2);
       fs.mkdir("result",err=>{
          if(err){
-	    console.log('warnining: A folder named "result" exists already and is ready for the placement of the "posts.json" file.')
+	    console.log('warnining: A folder named "result" exists already. ');
          }else{
-             console.log('A folder named "result" has been created and is ready for the placement of the "posts.json" file')
+             console.log('A folder named "result" has been created. ');
         }
       });
       fs.writeFile("./result/posts.json",jsonString,"utf8",err=>{
          if(err){
-	    console.log(err)
+	    console.log(err);
 	 }else{
 	     setTimeout(_=>{
-                console.log('A file named "posts.json" has been created inside the "result" folder. cd into "./result" to view the file')
+		console.log("Operation Successfull :)");
+                console.log('A file named "posts.json" has been created inside the "result" folder. cd into "./result" to view the file');
 	     },1500)
 	 }
       });
    })
    .catch(err=>{
-      if (err) console.log(err)
+      if (err) console.log(err);
    })
